25d7d355e36cf7f257586eaf8b6f5baae827fd92
sianniello <steno87@hotmail.com>
2017-08-16 08:55:27 +0200

    extended nat

---
+++ find.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Stefano on 16/08/2017.
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    db.collection("customers").find({},
+        {_id:false}).toArray(function(err, result) {
+        if(err) throw err;
+        console.log(result);
+        db.close()
+    });
+});
